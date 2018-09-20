398a399f52a62f80a65323578072132adbd52f6d
Charlie Hirsch <hirschcharlie@gmail.com>
2018-09-20 11:28:10 -0400

    start ProblemThree

---
+++ ProblemThree.java
@@ -0,0 +1,29 @@
+public class ProblemThree{
+  public static void main(String[] args){
+
+    double largestPrimeFactor = findLargestPrimeFactor(600851475143.0);
+
+
+    System.out.println("\n" + "Problem Three:");
+    System.out.println("What is the largest prime factor of the number 600851475143?");
+    System.out.println("The largest prime factor is " + String.format("%,f", largestPrimeFactor) + ".\n");
+
+  }
+
+    public static double findLargestPrimeFactor(double end){
+      double i = 2;
+      double toReturn = 0;
+      while (i<Math.sqrt(end)){
+        if (isPrime(i) && end%i==0){
+          toReturn=i;
+        }
+        i++;
+        System.out.println(i);
+      }
+      return toReturn;
+    }
+
+    public static boolean isPrime(double test){
+      fix and test every number
+    }
+}
