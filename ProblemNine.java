aef9414bbe0d5196d035109c8a4294ba75d3a29d
Charlie Hirsch <hirschcharlie@gmail.com>
2018-09-26 13:55:05 -0400

    Finish ProblemEight (no commas)

---
+++ ProblemNine.java
@@ -0,0 +1,5 @@
+public class ProblemNine{
+  public static void main(String[] args){
+  
+  }
+}
--- ProblemSeven.java
+++ ProblemSeven.java
@@ -4,7 +4,7 @@ public class ProblemSeven{
     System.out.println("\n" + "Problem Seven:");
     System.out.println("What is the 10 001st prime number?");
     System.out.println("The 10,001st prime is " + String.format("%,d", getPrime(10001)) + ".\n");
-    System.out.println(isPrime(104760));
+    
   }
 
   public static int getPrime(int number){
