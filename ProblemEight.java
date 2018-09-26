8f3b65b06a6ce5483dae8d9bbf6dbfd861d77ea5
Charlie Hirsch <hirschcharlie@gmail.com>
2018-09-26 14:09:43 -0400

    Fix names

--- ProblemEight.java
+++ ProblemEight.java
@@ -2,25 +2,8 @@ public class ProblemEight{
   public static void main(String[] args){
 
     System.out.println("\n" + "Problem Eight:");
-    System.out.println("There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.");
-    System.out.println("The product is " + getPythagTrip(1000) + ".\n");
+    System.out.println("Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?");
+    System.out.println("The product is " + String.format("%,d", "") + ".\n");    
 
   }
-
-    public static int getPythagTrip(int sum){
-
-      for (int a = 1; a<sum; a++){
-        for (int b = 2; b<sum; b++){
-          for (int c = 3; c<sum; c++){
-            if (a+b+c==sum && c>b && b>a && a*a + b*b == c*c){
-              return a*b*c;
-            }
-          }
-        }
-      }
-      return -1;
-    }
-
-
-
 }
--- ProblemNine.java
+++ ProblemNine.java
@@ -1,5 +1,26 @@
 public class ProblemNine{
   public static void main(String[] args){
-  
+
+    System.out.println("\n" + "Problem Nine:");
+    System.out.println("There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.");
+    System.out.println("The product is " + getPythagTrip(1000) + ".\n");
+
   }
+
+    public static int getPythagTrip(int sum){
+
+      for (int a = 1; a<sum; a++){
+        for (int b = 2; b<sum; b++){
+          for (int c = 3; c<sum; c++){
+            if (a+b+c==sum && c>b && b>a && a*a + b*b == c*c){
+              return a*b*c;
+            }
+          }
+        }
+      }
+      return -1;
+    }
+
+
+
 }
